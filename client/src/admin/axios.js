9488f288b635aeec8e903401132fede09a2c80b2
Davide Cioccia <daci@genmab.com>
2024-06-14 08:51:55 +0200

    logging

--- client/src/admin/axios.js
+++ client/src/admin/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 
 const accessToken = JSON.parse(localStorage.getItem('user'))?.accessToken || ''
+console.log('accessToken:', accessToken)
+console.log(localStorage.getItem('user'))
 const PORT = process.env.REACT_APP_PORT || 5000
 
 const instance = axios.create({
